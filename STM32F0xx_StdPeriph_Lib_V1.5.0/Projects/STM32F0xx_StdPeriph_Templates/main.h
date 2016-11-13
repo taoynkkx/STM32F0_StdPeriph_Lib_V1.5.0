589cb034033da951b3e8495686e0fe8e531d9413
Ricky Tao <taoynkkx@163.com>
2016-11-14 00:28:04 +0800

    add MDK4 project file add .gitignore

--- STM32F0xx_StdPeriph_Lib_V1.5.0/Projects/STM32F0xx_StdPeriph_Templates/main.h
+++ STM32F0xx_StdPeriph_Lib_V1.5.0/Projects/STM32F0xx_StdPeriph_Templates/main.h
@@ -32,16 +32,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx.h"
 
-#ifdef USE_STM320518_EVAL
- #include "stm320518_eval.h"
- #include "stm320518_eval_lcd.h"
-#endif /* USE_STM320518_EVAL */
-
-#ifdef USE_STM32072B_EVAL
- #include "stm32072b_eval.h"
- #include "stm32072b_eval_lcd.h"
-#endif /* USE_STM32072B_EVAL */
-
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
